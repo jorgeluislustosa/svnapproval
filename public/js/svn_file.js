7fc324658dfa83166c992a1534865a50d666fc94
Jorge Lustosa <jorgelustosa@gmail.com>
2012-01-26 18:56:55 +0000

    teste classe

--- public/js/svn_file.js
+++ public/js/svn_file.js
@@ -8,17 +8,17 @@ $(document).ready(function()
 
 	$.post('/svnapproval/file/svnst/', function(data) 
 	{
-		
-		
-		alert(data) ; 
-		
+
 		// when no have data to show !!! 
 		if(data=="")
 		{ 
 			$('#fileschange').hide();
 		}
+		else
+		{ 
+			$('#changedfiles').html(data);
+		}
 		
-		$('#changedfiles').html(data);
 		
 	});
 	
