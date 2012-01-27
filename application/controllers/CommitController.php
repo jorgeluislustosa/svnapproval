1d98e9226c8efbd6a9b26c4832834798b78f15f7
Jorge Lustosa <jorgelustosa@gmail.com>
2012-01-27 19:25:30 +0000

    new controllers

---
+++ application/controllers/CommitController.php
@@ -0,0 +1,55 @@
+<?php
+
+
+class CommitController extends Zend_Controller_Action
+{
+	
+	
+	public function init()
+	{
+		/* Initialize action controller here */
+		
+	}
+	
+	public function indexAction() 
+	{
+		
+		$project = $this->loadprojectdata() ; 
+		
+		$project->name  ;
+		$this->view->folder = $project->directory  ; 
+		$this->view->project_name = $project->name  ; 
+		
+		include("../library/phpSVNclient.php") ;
+		$project = $this->loadprojectdata() ;
+		$svn  = new phpsvnclient;
+		
+		$svn->setRespository($project->svn_url);
+		$svn->setAuth($project->svn_username,$project->svn_password) ;
+		$logs = $svn->getRepositoryLogs($svn->getVersion() - 100 );
+		
+		$this->view->logs  = $logs ;
+		
+		
+		
+	}
+	
+	
+	
+	private function loadprojectdata()
+	{ 
+		
+		$data = new Application_Model_DbTable_Projects() ;
+		$project = $data->find('1') ;
+		$row = $project->current();
+		return $row  ;
+		
+	}
+	
+
+	
+	
+	
+	
+} 
+?>
---
+++ public/js/svn_commit.js
@@ -0,0 +1,15 @@
+$(document).ready(function()
+{
+
+	// change  tab settings 
+	$("#files_tab").attr("class", "");
+	$("#commit_tab").attr("class", "selected");
+	
+	
+	
+	
+	
+});
+
+
+
